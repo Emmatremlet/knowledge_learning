36a3a099af1c7ff6dc08f8430bf08f7ddae983fb
Emma TREMLET <elecuelle001@ensc.fr>
2025-01-29 17:26:58 +0100

    feat: change index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,18 @@
 <?php
 
 use App\Kernel;
-
 use Symfony\Component\Dotenv\Dotenv;
 
+require dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+// Charger les variables d'environnement uniquement en local
 if (!isset($_SERVER['APP_ENV']) && file_exists(dirname(__DIR__).'/.env')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
-return function (array $context) {
-    return new Kernel($context['APP_ENV'] ?? 'prod', (bool) ($context['APP_DEBUG'] ?? false));
-};
+// Initialiser Symfony
+$kernel = new Kernel($_SERVER['APP_ENV'] ?? 'prod', (bool) ($_SERVER['APP_DEBUG'] ?? false));
+$request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
