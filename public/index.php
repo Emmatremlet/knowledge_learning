f3d925834231d6d0551ddb478a8a0e498621986b
Emma TREMLET <elecuelle001@ensc.fr>
2025-01-29 17:05:40 +0100

    Fix .env loading in production

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
+if (file_exists(dirname(__DIR__).'/.env') && $_SERVER['APP_ENV'] !== 'prod') {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+}
+
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
