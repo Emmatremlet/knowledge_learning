3a641b30f63e7e375e4d905a5c20760a3e9feddc
Emma TREMLET <elecuelle001@ensc.fr>
2025-01-30 19:17:06 +0100

    Change the register controller

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -15,6 +15,8 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordC
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
+use Psr\Log\LoggerInterface;
+
 
 /**
  * AppCustomAuthenticator
